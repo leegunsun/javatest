7a51e254e1a1527c4473c3062ed6a1c8e0a29c98
이건선 <igeonseon@igeonseon-ui-Macmini.local>
2025-02-13 13:40:01 +0900

    a

--- src/main/java/com/example/open/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/open/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.example.open.common.exception;
 
-import com.example.open.common.dto.ErrorResponse;
+import com.example.open.common.dto.ApiResponse;
 import com.example.open.common.service.ProfileCheckerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,14 +15,14 @@ public class GlobalExceptionHandler {
     private final ProfileCheckerService profileCheckerService;
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+    public ResponseEntity<ApiResponse<String>> handleGenericException(Exception ex) {
         String message = "서버 오류 입니다.";
 
         if(profileCheckerService.isDebug()) {
             message = ex.getCause().getMessage();
         }
 
-        return ResponseEntity.status((HttpStatus.INTERNAL_SERVER_ERROR)).body(new ErrorResponse(500, message));
+        return ResponseEntity.status((HttpStatus.INTERNAL_SERVER_ERROR)).body(ApiResponse.error(message));
     }
 
 }
--- src/main/java/com/example/open/common/service/ProfileCheckerService.java
+++ src/main/java/com/example/open/common/service/ProfileCheckerService.java
@@ -3,7 +3,6 @@ package com.example.open.common.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-import org.springframework.core.env.Profiles;
 
 @Service
 @RequiredArgsConstructor
