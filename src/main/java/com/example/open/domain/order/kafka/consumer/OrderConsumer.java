4444829adb651e393e0a3be1c162488f7a211aff
leegunsun <talkcjstk@naver.com>
2025-02-22 12:59:53 +0900

    카프카 비활성화

--- src/main/java/com/example/open/domain/order/kafka/consumer/OrderConsumer.java
+++ src/main/java/com/example/open/domain/order/kafka/consumer/OrderConsumer.java
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class OrderConsumer {
-    private final OrderService orderService;
-
-    public OrderConsumer(OrderService orderService) {
-        this.orderService = orderService;
-    }
-
-    @KafkaListener(topics = "order-events", groupId = "order-group")
-    public void processOrder(String orderId) {
-        System.out.println("🛒 주문 처리 중: " + orderId);
-        orderService.saveOrder(orderId);
-    }
+//    private final OrderService orderService;
+//
+//    public OrderConsumer(OrderService orderService) {
+//        this.orderService = orderService;
+//    }
+//
+//    @KafkaListener(topics = "order-events", groupId = "order-group")
+//    public void processOrder(String orderId) {
+//        System.out.println("🛒 주문 처리 중: " + orderId);
+//        orderService.saveOrder(orderId);
+//    }
 }
