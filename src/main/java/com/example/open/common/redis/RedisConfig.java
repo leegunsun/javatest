62f782bf3a02d455fd83d040f04311cac4d7333a
leegunsun <talkcjstk@naver.com>
2025-02-23 08:51:29 +0900

    클러스터 일단 성공

--- src/main/java/com/example/open/common/redis/RedisConfig.java
+++ src/main/java/com/example/open/common/redis/RedisConfig.java
@@ -2,26 +2,52 @@ package com.example.open.common.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.util.Arrays;
+
 @Configuration
 public class RedisConfig {
 
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+////        config.setPassword("your_redis_password");  // Redis에 비밀번호가 설정되어 있다면 입력합니다.
+//        return new LettuceConnectionFactory(config);
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate() {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(redisConnectionFactory());
+//        template.setKeySerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new StringRedisSerializer());
+//        return template;
+//    }
+
+@Bean
+public RedisClusterConfiguration redisClusterConfiguration() {
+    return new RedisClusterConfiguration(Arrays.asList(
+            "10.1.0.45:6379",
+            "10.1.0.46:6379",
+            "10.1.0.47:6379"
+    ));
+}
+
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
-//        config.setPassword("your_redis_password");  // Redis에 비밀번호가 설정되어 있다면 입력합니다.
-        return new LettuceConnectionFactory(config);
+    public LettuceConnectionFactory redisConnectionFactory(RedisClusterConfiguration redisClusterConfiguration) {
+        return new LettuceConnectionFactory(redisClusterConfiguration);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
